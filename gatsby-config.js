fef526b0974587591e59b95f41fa12a91c316809
nerraf-dev <87069314+nerraf-dev@users.noreply.github.com>
2021-11-02 21:27:08 +0100

    added staticImage

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Geeky Teacher",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
