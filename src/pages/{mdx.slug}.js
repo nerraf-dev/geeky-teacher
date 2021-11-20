8ae19e1bdad8cf5e3339ab3a36cced6d18c21df8
nerraf-dev <87069314+nerraf-dev@users.noreply.github.com>
2021-11-20 21:05:23 +0100

    dynamic blog pages

---
+++ src/pages/{mdx.slug}.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layoutut'
+
+const BlogPost = () => {
+    return(
+        <Layout pageTitle="Some posts...">
+            <p>Posts will end up here</p>
+        </Layout>
+    )
+}
+
+export default BlogPost
